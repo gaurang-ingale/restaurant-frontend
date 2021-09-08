35993b8bbfda1a6c5ada0a153c453537a971f3aa
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-09-08 17:44:24 +0200

    Create an Item component to display delicacies the restaurant serves

---
+++ src/components/Item.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+const Item = ({ name, picture, price, description, currency }) => {
+  return (
+    <article id="item-container">
+      <figure>
+        <img id="item-image" src={picture} alt=""></img>
+        <figcaption id="item-title">{name}</figcaption>
+      </figure>
+      <p id="item-description">{description}</p>
+      <p id="item-price">{currency + price}</p>
+    </article>
+  );
+};
+
+Item.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Item;
