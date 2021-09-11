d82330e535c99a71f3ad0cac051fbea9a9fa161b
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-09-11 20:14:08 +0200

    Fix styling for Menu page

--- components/Item.js
+++ components/Item.js
@@ -1,15 +1,18 @@
 import PropTypes from "prop-types";
 import styles from "./styles/Item.module.scss";
+import Image from "next/image";
 
 const Item = ({ name, picture, price, description, currency }) => {
   return (
     <article id={styles.item_container}>
-      <figure>
-        <img
+      <figure id={styles.image_container}>
+        <Image
           id={styles.item_image}
           src={"http://localhost:1337" + picture}
           alt=""
-        ></img>
+          width="250"
+          height="200"
+        />
         <figcaption id={styles.item_title}>{name}</figcaption>
       </figure>
       <p id={styles.item_description}>{description}</p>
