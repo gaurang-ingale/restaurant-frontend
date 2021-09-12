89bcba18d54c30e859ae4ce8e5d47139b57335af
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-09-12 12:33:42 +0200

    Write basic styles for the About page

---
+++ pages/about.js
@@ -0,0 +1,68 @@
+import styles from "./styles/About.module.scss";
+import Image from "next/image";
+
+const About = (props) => {
+  return (
+    <main id={styles.about_container}>
+      <h1 id={styles.about_title}>{props.restaurantTitle}</h1>
+      <article id={styles.about_article}>
+        <figure>
+          <Image
+            width="400"
+            height="500"
+            src={"http://localhost:1337" + props.image}
+            alt=""
+          />
+        </figure>
+        <section id={styles.about_description}>
+          <h2>{props.restaurantStoryTitle}</h2>
+          <p>{props.restaurantStory}</p>
+        </section>
+      </article>
+      <footer id={styles.about_contact}>
+        <section id={styles.email_phone}>
+          <p>{props.email}</p>
+          <p>{props.phone}</p>
+        </section>
+        <address>{props.address}</address>
+      </footer>
+    </main>
+  );
+};
+
+export async function getStaticProps(context) {
+  const result = await fetch("http://localhost:1337/about");
+  const data = await result.json();
+
+  if (!data) {
+    return {
+      props: {
+        error: true,
+      },
+      revalidate: 600, //revalidate every 10 minutes max
+    };
+  }
+
+  const restaurantTitle = data.RestaurantTitle;
+  const restaurantStoryTitle = data.StoryTitle;
+  const restaurantStory = data.Story;
+  const image = data.Image.url;
+  const phone = data.Phone;
+  const email = data.Email;
+  const address = data.Address;
+
+  return {
+    props: {
+      restaurantTitle,
+      restaurantStoryTitle,
+      restaurantStory,
+      image,
+      phone,
+      email,
+      address,
+    },
+    revalidate: 600, //revalidate every 10 minutes max
+  };
+}
+
+export default About;
